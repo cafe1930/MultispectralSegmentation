0f0f72753d886e7a49c1ec10c48455eabd785fc4
cafe1930 <m.y.uzdiaev@gmail.com>
2025-11-04 01:48:20 +0300

    add patch attention config

--- train_nn.py
+++ train_nn.py
@@ -404,7 +404,8 @@ if __name__ == '__main__':
         #'training_configs/models/fcn1.yaml',
         #'training_configs/models/unet.yaml',
         #'training_configs/models/unet_hsi.yaml',
-        'training_configs/models/unet_aux_tr_hsi.yaml',
+        #'training_configs/models/unet_aux_chtr_hsi.yaml',
+        'training_configs/models/unet_aux_patr_hsi.yaml',
 
         '--paths_to_encoder_configs',
         #'training_configs/encoders/tu-maxvit_tiny.yaml',
